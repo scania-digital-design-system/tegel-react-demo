19d3e4ab1fcd9943f1252d2f688585db550d5d65
mJarsater <martin.jarsater@hotmail.com>
2023-01-31 09:19:45 +0100

    Angular setup

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,7 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import  { defineCustomElements } from '@scania/tegel/loader'
 import { AppModule } from './app/app.module';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+defineCustomElements(window);
