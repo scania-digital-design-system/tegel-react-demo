ddea230c5cf7df86eece84c91d972c848e30ac4b
mJarsater <martin.jarsater@hotmail.com>
2023-03-01 08:46:25 +0100

    block components

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -37,6 +37,35 @@ function App() {
             link-target="_self"
           ></sdds-banner>
         </div>
+        <sdds-block>
+          <div className="sdds-headline-01">Headline</div>
+          <p className="sdds-body-01">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+            condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+            sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+            consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+            rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+            Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit leo
+            at diam ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet
+            rhoncus arcu. Sed sed molestie elit. Nullam in interdum est, vitae
+            aliquam ipsum. Nunc rutrum nibh ut arcu egestas egestas.
+          </p>
+          <sdds-block>
+            <div className="sdds-headline-01">Headline</div>
+            <p className="sdds-body-01">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+              sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+              consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+              rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+              Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit
+              leo at diam ullamcorper, vitae volutpat neque dapibus. Maecenas
+              sit amet rhoncus arcu. Sed sed molestie elit. Nullam in interdum
+              est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu egestas
+              egestas.
+            </p>
+          </sdds-block>
+        </sdds-block>
       </main>
     </div>
   );
