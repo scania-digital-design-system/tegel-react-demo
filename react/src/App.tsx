2ca98e5a02e379410e33659291e6c98045639589
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-03-02 10:45:50 +0100

    feat(block): integrated component (#4)
    * block components
    
    * added headline for block

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -37,6 +37,38 @@ function App() {
             link-target="_self"
           ></sdds-banner>
         </div>
+        <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Block</div>
+        <sdds-block>
+          <div className="sdds-headline-01">Headline</div>
+          <p className="sdds-body-01">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+            condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+            sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+            consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+            rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+            Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit leo
+            at diam ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet
+            rhoncus arcu. Sed sed molestie elit. Nullam in interdum est, vitae
+            aliquam ipsum. Nunc rutrum nibh ut arcu egestas egestas.
+          </p>
+          <sdds-block>
+            <div className="sdds-headline-01">Headline</div>
+            <p className="sdds-body-01">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+              condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+              sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+              consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+              rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+              Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit
+              leo at diam ullamcorper, vitae volutpat neque dapibus. Maecenas
+              sit amet rhoncus arcu. Sed sed molestie elit. Nullam in interdum
+              est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu egestas
+              egestas.
+            </p>
+          </sdds-block>
+        </sdds-block>
+        </div>
       </main>
     </div>
   );
