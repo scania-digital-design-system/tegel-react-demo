36b9b3a7b4c788ed88b85e016f9e31cdac5b977d
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-03-07 16:54:46 +0100

    fix(header): active state

--- react/src/components/Header.tsx
+++ react/src/components/Header.tsx
@@ -1,4 +1,18 @@
+//https://medium.com/how-to-react/add-an-active-classname-to-the-link-using-react-router-b7c350473916
+
+import { useLocation } from "react-router-dom";
+
 const Header = () => {
+
+  //assigning location variable
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split("/");
+
   return (
     <>
       <nav className="sdds-nav sdds-u-sticky sdds-u-top0">
@@ -7,12 +21,12 @@ const Header = () => {
         </div>
         <div className="sdds-nav__center">
           <ul className="sdds-nav__inline-menu">
-            <li className="sdds-nav__item">
+            <li className={splitLocation[1] === "" || splitLocation[1] === "web-components" ? "sdds-nav__item sdds-nav__item--active" : "sdds-nav__item" }>
               <a className="sdds-nav__item-core" href="/web-components">
                 <span className="sdds-nav__item-core-text">Web</span>
               </a>
             </li>
-            <li className="sdds-nav__item">
+            <li className={(splitLocation[1] === "native-components") ? "sdds-nav__item sdds-nav__item--active" : "sdds-nav__item" }>
               <a className="sdds-nav__item-core" href="/native-components">
                 <span className="sdds-nav__item-core-text">Native</span>
               </a>
@@ -23,7 +37,7 @@ const Header = () => {
           {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
           <a
             className="sdds-nav__item sdds-nav__app-logo"
-            href="https://www.scania.com"
+            href="/"
           ></a>
         </div>
       </nav>
