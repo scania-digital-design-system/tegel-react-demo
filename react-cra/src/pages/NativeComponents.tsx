22dc37ce8558441fa4f1f1f7f1c26101de02badc
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-03-10 14:19:44 +0100

    feat(mode): adding native component pairs (#34)
    * feat(mode): adding native banner
    
    * feat(mode): adding native block
    
    * feat(mode): adding native breadcrumb
    
    * feat(mode): adding native buttons
    
    * feat(mode): adding native card
    
    * feat(mode): adding native checkbox
    
    * feat(mode): adding native message
    
    * fix(radio-button): position update
    
    * chore: eslint update

--- react-cra/src/pages/NativeComponents.tsx
+++ react-cra/src/pages/NativeComponents.tsx
@@ -7,35 +7,110 @@ export default function NativeComponents() {
   return (
     <>
       <div>
-        <div className="sdds-headline-02 sdds-u-pb1">Radio Button</div>
-        <div className="sdds-radio-button-group">
-          <div className="sdds-radio-item">
-            <input
-              className="sdds-form-input"
-              type="radio"
-              name="rb-example"
-              id="rb-option-1"
-              defaultChecked={true}
-            />
-            <label className="sdds-form-label" htmlFor="rb-option-1">
-              Label text 1
-            </label>
+        <div className="sdds-headline-02 sdds-u-pb1">Banner</div>
+        <div className="sdds-banner sdds-banner-error">
+          <span className="sdds-banner-prefix"><sdds-icon name="warning" size="20px"></sdds-icon></span>
+          <div className="sdds-banner-body">
+            <h6 className="sdds-banner-header">Warning</h6>
+            <div className="sdds-banner-subheader">Insufficient amount of memory</div>
+            <a className="sdds-link sdds-banner-link" href="https://www.scania.com" target="_self" rel="noreferrer">Click her to read more</a>
           </div>
+          <button type="button" aria-label="close" className="sdds-banner-close">
+            <sdds-icon name="cross" size="20px"></sdds-icon>
+          </button>
         </div>
-        <div className="sdds-radio-button-group">
-          <div className="sdds-radio-item">
-            <input
-              className="sdds-form-input"
-              type="radio"
-              name="rb-example"
-              id="rb-option-2"
-            />
-            <label className="sdds-form-label" htmlFor="rb-option-2">
-              Label text 2
-            </label>
+      </div>
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Block</div>
+        <div className="sdds-block">
+          <h2 className="sdds-headline-02">Block</h2>
+          <p className="sdds-body-01">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nisi ut eleifend
+            ultrices. Nunc venenatis maximus sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+            consectetur interdum. Vestibulum condimentum, augue vitae lobortis rhoncus, mi est ultricies mi,
+            sed tincidunt magna nibh in lectus. Pellentesque vel vulputate orci, vel lacinia orci. Sed
+            suscipit leo at diam ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet rhoncus arcu.
+            Sed sed molestie elit. Nullam in interdum est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu
+            egestas egestas.
+          </p>
+          <div className="sdds-block">
+            <h3 className="sdds-headline-04">Nested block</h3>
+            <p className="sdds-detail-03">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nisi ut eleifend
+              ultrices. Nunc venenatis maximus sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+              consectetur interdum. Vestibulum condimentum, augue vitae lobortis rhoncus, mi est ultricies
+              mi, sed tincidunt magna nibh in lectus. Pellentesque vel vulputate orci, vel lacinia orci. Sed
+              suscipit leo at diam ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet rhoncus
+              arcu. Sed sed molestie elit. Nullam in interdum est, vitae aliquam ipsum. Nunc rutrum nibh ut
+              arcu egestas egestas.
+            </p>
           </div>
         </div>
       </div>
+
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Breadcrumb</div>
+        <div className="sdds-breadcrumb">
+          <div className="sdds-breadcrumb-item"><a href="https://www.scania.com" >Page 1</a></div>
+          <div className="sdds-breadcrumb-item"><a href="https://www.scania.com" >Page 2</a></div>
+          <div className="sdds-breadcrumb-item sdds-breadcrumb-item-current"><a href="https://www.scania.com" aria-current="page">Page 3</a></div>
+        </div>
+      </div>
+
+      <div className="sdds-u-w-100">
+        <div className="sdds-headline-02 sdds-u-pb1">Buttons</div>
+        <button className="sdds-btn sdds-btn-primary sdds-btn-lg sdds-btn-fullbleed">
+          <span className="sdds-btn-text">Primary fullbleed button</span>
+        </button>
+        <div
+            className="sdds-u-mt2 sdds-u-flex"
+            style={{ justifyContent: "space-around" }}
+        >
+          <button className="sdds-btn sdds-btn-danger sdds-btn-md">
+            <span className="sdds-btn-text">Delete file</span>
+            <i className="sdds-btn-icon sdds-icon trash"></i>
+          </button>
+          <button className="sdds-btn sdds-btn-ghost sdds-btn-sm sdds-btn-icon sdds-btn-only-icon">
+            <i className="sdds-btn-icon sdds-icon save"></i>
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <div className="sdds-card">
+          <img
+              className="sdds-card-img"
+              src="https://images.unsplash.com/photo-1670509295484-df0c2512fec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=5494&q=80"
+              alt="Truck on the road"
+          />
+          <div className="sdds-card-header-avatar">
+            <div className="sdds-card-avatar">
+              <img
+                  src="https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80"
+                  alt="Person avatar"
+              />
+            </div>
+            <div className="sdds-card-headlines">
+              <h6 className="sdds-card-headline">Header text</h6>
+              <h6 className="sdds-card-sub-headline">Subheader text</h6>
+            </div>
+          </div>
+          <div className="sdds-card-body">Hello in a body text</div>
+          <div className="sdds-card-footer">
+            <sdds-icon style={{fontSize: '20px'}} name="arrow_right"></sdds-icon>
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Checkbox</div>
+        <div className="sdds-checkbox-item">
+          <input className="sdds-form-input" type="checkbox" id="native-checkbox"/>
+          <label className="sdds-form-label" htmlFor="native-checkbox">Label</label>
+        </div>
+      </div>
+
+
 
       <div>
         <div className="sdds-headline-02 sdds-u-pb1">Chip</div>
@@ -70,6 +145,25 @@ export default function NativeComponents() {
         <a href="/" className="sdds-link">
           This is a link.
         </a>
+      </div>
+
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Message</div>
+        <div
+            className="sdds-message sdds-message-type-error sdds-message-icon-active sdds-message-extended-active"
+        >
+          <div>
+            <sdds-icon
+                className="sdds-message-icon sdds-message-icon-error"
+                name="error"
+                size="20"
+            ></sdds-icon>
+          </div>
+          <h4 className="sdds-message-single">Single line message goes here.</h4>
+          <p className="sdds-message-extended">
+            Longer message text can be placed here. Longer message text can be placed here.
+          </p>
+        </div>
       </div>
       <div>
         <div className="sdds-headline-02 sdds-u-pb1">Modal</div>
@@ -123,6 +217,38 @@ export default function NativeComponents() {
           </div>
         </div>
       </div>
+
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Radio Button</div>
+        <div className="sdds-radio-button-group">
+          <div className="sdds-radio-item">
+            <input
+                className="sdds-form-input"
+                type="radio"
+                name="rb-example"
+                id="rb-option-1"
+                defaultChecked={true}
+            />
+            <label className="sdds-form-label" htmlFor="rb-option-1">
+              Label text 1
+            </label>
+          </div>
+        </div>
+        <div className="sdds-radio-button-group">
+          <div className="sdds-radio-item">
+            <input
+                className="sdds-form-input"
+                type="radio"
+                name="rb-example"
+                id="rb-option-2"
+            />
+            <label className="sdds-form-label" htmlFor="rb-option-2">
+              Label text 2
+            </label>
+          </div>
+        </div>
+      </div>
+
       <div>
         <div className="sdds-headline-02 sdds-u-pb1">Toast</div>
         <div className="sdds-toast sdds-toast-success">
@@ -163,6 +289,7 @@ export default function NativeComponents() {
           </label>
         </div>
       </div>
+
     </>
   );
 }
--- react-cra/src/pages/WebComponents.tsx
+++ react-cra/src/pages/WebComponents.tsx
@@ -131,8 +131,8 @@ export default function WebComponents() {
           header="Card title"
           header-placement="below"
           subheader="Subheader text"
-          header-img="https://scdn.reflex.prod.aws.scania.com/avatar/VqsHAtBNOQvjoslmKEv1599CCQOYYv-250.png?Expires=1679812312&Key-Pair-Id=K2MBMFK028C481&Signature=VUIW8gjppf9r8HjL2zl77AHyYPsFCUv7o7nQLkTgY1IZ~Ja3MkUvslGFnxx-XBneQo9y4IVZDDNBYsMxT0D0pBxnNrt5q3k65q5mZh8MTS15PznVxS7KKP~F7vGngaVsHeJbNHZ~iEytTaezBmX6Yzzng~47PXvZyw5lMohGUNY7yY4d0T5sfbwjPMfZrqUQWWxBxYl9icWOJ2eGPpPol6JSRKlMJMwf4wgNsYKEIoJaIh-sYinGl9reaW6Mv215qrbqdUf8aOiX48-bSBbJsGrUnIJlPGrq7-1vPg9c9qeteIxWqF7fUaj9~z342TIRvbNiozZ3CHDve2QGWpzyow__"
-          body-img="https://scdn.reflex.prod.aws.scania.com/thumbnails/2.6a4c8c88169b49c419d1a8/18.12bc286418679e166a96d74.webp?Expires=1678251771&Key-Pair-Id=K2MBMFK028C481&Signature=Zq51cDQl-92BG136TnDH57Lf46CXNdlI2WAIfLmg~NsYqITvoTMtCJpb5wJLPEMmDY~ufJMbJmaKoMo93yZVZSXihFehAk40slf8v40czQ1uMydC-85vYlItEy~5cSeu8wsLWRXyUCTv7BDlHQ2OWxFXjv0QMR2ggoHb1cBsElWshMILTm69VwrMtKutfGQz9~cLG0ApjeyOPmTBMbI4SU0FexiNCDUmjWAuLGvj2I02ExxetbUQU8pcddMgnsnfubRCzNpK2b~pQBNGIA4yYjqywPElVGQG0B7Xk8J9XY70gQRYXVZph6sT-umLb95TGPymRZoczmFgtzxg4xYHqg__"
+          header-img="https://images.unsplash.com/photo-1499887142886-791eca5918cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80  "
+          body-img="https://images.unsplash.com/photo-1670509295484-df0c2512fec4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=5494&q=80"
           clickable
         >
           <div slot="card-body">Here is a short description in body text</div>
@@ -273,6 +273,27 @@ export default function WebComponents() {
           </button>
         </sdds-modal>
       </div>
+
+      <div>
+        <div className="sdds-headline-02 sdds-u-pb1">Radio Button</div>
+        <sdds-radio-button
+            name="rb-example"
+            value="option1"
+            radio-id="option-1"
+            checked
+        >
+          Label text 1
+        </sdds-radio-button>
+
+        <sdds-radio-button
+            name="rb-example"
+            value="option2"
+            radio-id="option-2"
+        >
+          Label text 2
+        </sdds-radio-button>
+      </div>
+
       <div>
         <div className="sdds-headline-02 sdds-u-pb1">Popover</div>
         <div className="sdds-headline-03 sdds-u-pb1">Canvas</div>
@@ -375,25 +396,7 @@ export default function WebComponents() {
           </ul>
         </sdds-popover-menu>
       </div>
-      <div>
-        <div className="sdds-headline-02 sdds-u-pb1">Radio Button</div>
-        <sdds-radio-button
-          name="rb-example"
-          value="option1"
-          radio-id="option-1"
-          checked
-        >
-          Label text 1
-        </sdds-radio-button>
 
-        <sdds-radio-button
-          name="rb-example"
-          value="option2"
-          radio-id="option-2"
-        >
-          Label text 2
-        </sdds-radio-button>
-      </div>
       <div>
         <div className="sdds-headline-02 sdds-u-pb1">Slider</div>
         <sdds-slider
