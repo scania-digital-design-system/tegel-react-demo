4e369b12802baf0db585277310ce17047770b161
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-03-14 15:29:44 +0100

    Added native footer to native components page (#41)

--- react-cra/src/components/Footer.tsx
+++ react-cra/src/components/Footer.tsx
@@ -1,26 +1,69 @@
+import { useLocation } from "react-router-dom";
+
 export default function Footer() {
-  return(
-      <sdds-footer>
-        <div slot="main-left">
-          <sdds-footer-link-group>
-            <sdds-footer-link link-href="#">Link text</sdds-footer-link>
-            <sdds-footer-link link-href="#">Link text</sdds-footer-link>
-            <sdds-footer-link link-href="#">Link text</sdds-footer-link>
-          </sdds-footer-link-group>
+  //assigning location variable
+  const location = useLocation();
+
+  //destructuring pathname from location
+  const { pathname } = location;
+
+  //Javascript split method to get the name of the path in array
+  const splitLocation = pathname.split("/");
+  console.log(splitLocation);
+  return splitLocation[1] === "native-components" ? (
+    <div>
+      <div className="sdds-footer">
+        <div className="sdds-footer-main">
+          <ul className="sdds-footer-main-links">
+            <li>
+              <a href="/">Legal link</a>
+            </li>
+            <li>
+              <a href="/">Legal link</a>
+            </li>
+            <li>
+              <a href="/">Legal link</a>
+            </li>
+          </ul>
+          <ul className="sdds-footer-social-links">
+            <li>
+              <a href="/">Social link</a>
+            </li>
+            <li>
+              <a href="/">Social link</a>
+            </li>
+            <li>
+              <a href="/">Social link</a>
+            </li>
+          </ul>
+          <div className="sdds-footer-main-brand">
+            <p>Copyright &copy; 2022 Scania</p>
+          </div>
         </div>
-        <div slot="main-right">
-          <sdds-footer-link-group>
-            <sdds-footer-link link-href="#">
-              <sdds-icon name="truck"></sdds-icon>
-            </sdds-footer-link>
-            <sdds-footer-link link-href="#">
-              <sdds-icon name="truck"></sdds-icon>
-            </sdds-footer-link>
-            <sdds-footer-link link-href="#">
-              <sdds-icon name="truck"></sdds-icon>
-            </sdds-footer-link>
-          </sdds-footer-link-group>
-        </div>
-      </sdds-footer>
+      </div>
+    </div>
+  ) : (
+    <sdds-footer>
+      <div slot="main-left">
+        <sdds-footer-link-group>
+          <sdds-footer-link link-href="#">Link text</sdds-footer-link>
+          <sdds-footer-link link-href="#">Link text</sdds-footer-link>
+          <sdds-footer-link link-href="#">Link text</sdds-footer-link>
+        </sdds-footer-link-group>
+      </div>
+      <div slot="main-right">
+        <sdds-footer-link-group>
+          <sdds-footer-link link-href="#">
+            <sdds-icon name="truck"></sdds-icon>
+          </sdds-footer-link>
+          <sdds-footer-link link-href="#">
+            <sdds-icon name="truck"></sdds-icon>
+          </sdds-footer-link>
+          <sdds-footer-link link-href="#">
+            <sdds-icon name="truck"></sdds-icon>
+          </sdds-footer-link>
+        </sdds-footer-link-group>
+      </div>
+    </sdds-footer>
   );
 }
