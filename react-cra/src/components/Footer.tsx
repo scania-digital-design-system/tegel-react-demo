6b2ab7d171c33aae0890afa00fe92fab0740203e
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-05-09 11:20:36 +0200

    updated tegel dependacy and removed native components (#55)

--- react-cra/src/components/Footer.tsx
+++ react-cra/src/components/Footer.tsx
@@ -1,48 +1,5 @@
-import { useLocation } from "react-router-dom";
-
 export default function Footer() {
-  //assigning location variable
-  const location = useLocation();
-
-  //destructuring pathname from location
-  const { pathname } = location;
-
-  //Javascript split method to get the name of the path in array
-  const splitLocation = pathname.split("/");
-  console.log(splitLocation);
-  return splitLocation[1] === "native-components" ? (
-    <div>
-      <div className="sdds-footer">
-        <div className="sdds-footer-main">
-          <ul className="sdds-footer-main-links">
-            <li>
-              <a href="/">Legal link</a>
-            </li>
-            <li>
-              <a href="/">Legal link</a>
-            </li>
-            <li>
-              <a href="/">Legal link</a>
-            </li>
-          </ul>
-          <ul className="sdds-footer-social-links">
-            <li>
-              <a href="/">Social link</a>
-            </li>
-            <li>
-              <a href="/">Social link</a>
-            </li>
-            <li>
-              <a href="/">Social link</a>
-            </li>
-          </ul>
-          <div className="sdds-footer-main-brand">
-            <p>Copyright &copy; 2022 Scania</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  ) : (
+  return (
     <sdds-footer>
       <div slot="main-left">
         <sdds-footer-group>
@@ -63,13 +20,19 @@ export default function Footer() {
       <div slot="main-right">
         <sdds-footer-group>
           <sdds-footer-item>
-            <a href="/"><sdds-icon name="truck"></sdds-icon></a>
+            <a href="/">
+              <sdds-icon name="truck"></sdds-icon>
+            </a>
           </sdds-footer-item>
           <sdds-footer-item>
-            <a href="/"><sdds-icon name="truck"></sdds-icon></a>
+            <a href="/">
+              <sdds-icon name="truck"></sdds-icon>
+            </a>
           </sdds-footer-item>
           <sdds-footer-item>
-            <a href="/"><sdds-icon name="truck"></sdds-icon></a>
+            <a href="/">
+              <sdds-icon name="truck"></sdds-icon>
+            </a>
           </sdds-footer-item>
         </sdds-footer-group>
       </div>
--- react-cra/src/components/Header.tsx
+++ react-cra/src/components/Header.tsx
@@ -1,47 +1,76 @@
-//https://medium.com/how-to-react/add-an-active-classname-to-the-link-using-react-router-b7c350473916
-
-import { Link, useLocation } from "react-router-dom";
-
 const Header = () => {
 
-  //assigning location variable
-  const location = useLocation();
-
-  //destructuring pathname from location
-  const { pathname } = location;
-
-  //Javascript split method to get the name of the path in array
-  const splitLocation = pathname.split("/");
+  let sideMenu: any;
 
   return (
     <>
-      <nav className="sdds-nav sdds-u-top0 sdds-u-absolute">
-        <div className="sdds-nav__left">
-          <div className="sdds-nav__app-name">React Demo</div>
-        </div>
-        <div className="sdds-nav__center">
-          <ul className="sdds-nav__inline-menu">
-            <li className={splitLocation[1] === "" || splitLocation[1] === "web-components" ? "sdds-nav__item sdds-nav__item--active" : "sdds-nav__item" }>
-              <Link className="sdds-nav__item-core" to="/web-components">
-                <span className="sdds-nav__item-core-text">Web</span>
-              </Link>
-            </li>
-            <li className={(splitLocation[1] === "native-components") ? "sdds-nav__item sdds-nav__item--active" : "sdds-nav__item" }>
-            <Link className="sdds-nav__item-core" to="/native-components">
-                <span className="sdds-nav__item-core-text">Native</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className="sdds-nav__right">
-          {/* eslint-disable-next-line jsx-a11y/anchor-has-content */}
-          <a
-            className="sdds-nav__item sdds-nav__app-logo"
-            href="/"
-          ></a>
-        </div>
-      </nav>
+      <sdds-header className="demo-header">
+        <sdds-header-hamburger
+          aria-expanded="false"
+          aria-label="Open application drawer"
+          aria-haspopup="true"
+          onClick={() => {
+            sideMenu.open = true;
+          }}
+        ></sdds-header-hamburger>
 
+        <sdds-header-title>My Application</sdds-header-title>
+        <sdds-header-brand-symbol
+          slot="end"
+          link-href="https://scania.com"
+          aria-label="Scania - red gryphon on blue shield"
+        ></sdds-header-brand-symbol>
+      </sdds-header>
+
+      <sdds-side-menu ref={(element) => sideMenu = element} aria-label="Side menu" id="demo-side-menu" persistent>
+        <sdds-side-menu-overlay
+          slot="overlay"
+        ></sdds-side-menu-overlay>
+
+        <sdds-side-menu-close-button
+          slot="close-button"
+          aria-label="Close drawer menu"
+          onClick={() => {
+            sideMenu.open = false;
+          }}
+        ></sdds-side-menu-close-button>
+
+        <sdds-side-menu-item>
+          <button>
+            <sdds-icon name="timer" size="24"></sdds-icon>
+            About us
+          </button>
+        </sdds-side-menu-item>
+
+        <sdds-side-menu-item>
+          <button>
+            <sdds-icon name="truck" size="24"></sdds-icon>
+            Trucks
+          </button>
+        </sdds-side-menu-item>
+
+        <sdds-side-menu-dropdown default-open selected>
+          <sdds-icon slot="button-icon" name="profile" size="24"></sdds-icon>
+          <span slot="button-label">Wheel types</span>
+          <sdds-side-menu-dropdown-list>
+            <sdds-side-menu-dropdown-list-item>
+              <a href="https://www.scania.com">Hub-centric wheel</a>
+            </sdds-side-menu-dropdown-list-item>
+            <sdds-side-menu-dropdown-list-item selected>
+              <a href="https://www.scania.com" aria-current="page">
+                Rim wheel
+              </a>
+            </sdds-side-menu-dropdown-list-item>
+          </sdds-side-menu-dropdown-list>
+        </sdds-side-menu-dropdown>
+
+        <sdds-side-menu-item>
+          <button>
+            <sdds-icon name="star" size="24"></sdds-icon>
+            Values
+          </button>
+        </sdds-side-menu-item>
+      </sdds-side-menu>
     </>
   );
 };
