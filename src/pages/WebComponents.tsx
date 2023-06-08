0fde4c191bd3dc5a935d891003215c9c647ce75b
mJarsater <martin.jarsater@hotmail.com>
2023-06-08 13:46:27 +0200

    added ignore rule for warning on link

--- src/pages/WebComponents.tsx
+++ src/pages/WebComponents.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import Banner from "../components/Banner";
 import DataTable from "../components/DataTable";
 export default function WebComponents() {
