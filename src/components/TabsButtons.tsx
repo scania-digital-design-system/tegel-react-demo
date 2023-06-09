f122c6d5b37f5a2fd736638af7c344df50bd4e55
mJarsater <martin.jarsater@hotmail.com>
2023-06-09 09:55:00 +0200

    added tabs with buttons and with links

---
+++ src/components/TabsButtons.tsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import TabsPanels from "./TabsPanels";
+
+const TabsButtons = () => {
+  const [folderTabIndex, setFolderTabIndex] = useState(0);
+  const [inlineTabIndex, setInlineTabIndex] = useState(0);
+  const [navigationTabIndex, setNavigationTabIndex] = useState(0);
+  return (
+    <div>
+      <h3>Tabs with buttons.</h3>
+      <p>
+        These tabs are using buttons and can be used to show/hide content.
+      </p>
+      <div className="tds-u-mb3 tabs">
+        <tds-folder-tabs
+          mode-variant="secondary"
+          ref={(element) =>
+            element?.addEventListener("tdsChange", (event: any) => {
+              setFolderTabIndex(event.detail.selectedTabIndex);
+              console.log(event);
+            })
+          }
+        >
+          <tds-folder-tab>
+            <button>First tab</button>
+          </tds-folder-tab>
+          <tds-folder-tab>
+            <button>Second tab is much longer</button>
+          </tds-folder-tab>
+          <tds-folder-tab>
+            <button>Third tab</button>
+          </tds-folder-tab>
+          <tds-folder-tab disabled>
+            <button>Fourth tab</button>
+          </tds-folder-tab>
+        </tds-folder-tabs>
+        <TabsPanels index={folderTabIndex} />
+      </div>
+      <div className="tds-u-mb3 tabs">
+        <tds-inline-tabs
+          mode-variant="secondary"
+          ref={(element) =>
+            element?.addEventListener("tdsChange", (event: any) => {
+              setInlineTabIndex(event.detail.selectedTabIndex);
+              console.log(event);
+            })
+          }
+        >
+          <tds-inline-tab>
+            <button>First tab</button>
+          </tds-inline-tab>
+          <tds-inline-tab>
+            <button>Second tab is much longer</button>
+          </tds-inline-tab>
+          <tds-inline-tab>
+            <button>Third tab</button>
+          </tds-inline-tab>
+          <tds-inline-tab disabled>
+            <button>Fourth tab</button>
+          </tds-inline-tab>
+        </tds-inline-tabs>
+        <TabsPanels index={inlineTabIndex} />
+      </div>
+      <div className="tds-u-mb3 tabs">
+        <tds-navigation-tabs
+          mode-variant="secondary"
+          ref={(element) =>
+            element?.addEventListener("tdsChange", (event: any) => {
+              setNavigationTabIndex(event.detail.selectedTabIndex);
+              console.log(event);
+            })
+          }
+        >
+          <tds-navigation-tab>
+            <button>First tab</button>
+          </tds-navigation-tab>
+          <tds-navigation-tab>
+            <button>Second tab is much longer</button>
+          </tds-navigation-tab>
+          <tds-navigation-tab>
+            <button>Third tab</button>
+          </tds-navigation-tab>
+          <tds-navigation-tab disabled>
+            <button>Fourth tab</button>
+          </tds-navigation-tab>
+        </tds-navigation-tabs>
+        <TabsPanels index={navigationTabIndex} />
+      </div>
+    </div>
+  );
+};
+
+export default TabsButtons;
