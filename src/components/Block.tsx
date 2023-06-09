93f7abe3c4d38a3409e8ecb3163d9260cc06d3e3
Tim <tim.romberg@scania.com>
2023-06-09 12:37:22 +0200

    feat: added block as separate component

---
+++ src/components/Block.tsx
@@ -0,0 +1,53 @@
+const Block = () => {
+    return (
+        <div>
+            <div className="tds-headline-02 tds-u-pb1">Block</div>
+            <tds-block>
+            <div className="tds-headline-01">Headline</div>
+            <p className="tds-body-01">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+                sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+                consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+                rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+                Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit leo
+                at diam ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet
+                rhoncus arcu. Sed sed molestie elit. Nullam in interdum est, vitae
+                aliquam ipsum. Nunc rutrum nibh ut arcu egestas egestas.
+            </p>
+            <tds-block>
+                <div className="tds-headline-01">Headline</div>
+                <p className="tds-body-01">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+                sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+                consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+                rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+                Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit
+                leo at diam ullamcorper, vitae volutpat neque dapibus. Maecenas
+                sit amet rhoncus arcu. Sed sed molestie elit. Nullam in interdum
+                est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu egestas
+                egestas.
+                </p>
+                <tds-block>
+                <div className="tds-headline-01">Headline</div>
+                <p className="tds-body-01">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
+                    condimentum nisi ut eleifend ultrices. Nunc venenatis maximus
+                    sapien, ac bibendum nisl aliquam in. Morbi ac velit et ligula
+                    consectetur interdum. Vestibulum condimentum, augue vitae
+                    lobortis rhoncus, mi est ultricies mi, sed tincidunt magna nibh
+                    in lectus. Pellentesque vel vulputate orci, vel lacinia orci.
+                    Sed suscipit leo at diam ullamcorper, vitae volutpat neque
+                    dapibus. Maecenas sit amet rhoncus arcu. Sed sed molestie elit.
+                    Nullam in interdum est, vitae aliquam ipsum. Nunc rutrum nibh ut
+                    arcu egestas egestas.
+                </p>
+                </tds-block>
+            </tds-block>
+            </tds-block>
+      </div>
+    )
+}
+
+export default Block;
