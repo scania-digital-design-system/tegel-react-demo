b57e343f5a7674e9b6c43940712769682e0842b3
mJarsater <martin.jarsater@hotmail.com>
2023-06-19 16:42:11 +0200

    updated basic table

--- src/components/Table/BasicTable.tsx
+++ src/components/Table/BasicTable.tsx
@@ -1,152 +1,70 @@
 const BasicTable = () => {
-    return (
-        <tds-table no-min-width vertical-dividers="false" compact-design="false" enable-responsive>
-  <tds-table-header>
-    <tds-header-cell column-key="truck" column-title="Truck type"></tds-header-cell>
-    <tds-header-cell column-key="driver" column-title="Driver name"></tds-header-cell>
-    <tds-header-cell column-key="country" column-title="Country"></tds-header-cell>
-    <tds-header-cell
-      column-key="mileage"
-      column-title="Mileage"
-      text-align="right"
-    ></tds-header-cell>
-  </tds-table-header>
-  <tds-table-body>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 1"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 2"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 3"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 4"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 5"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 6"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 7"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 8"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 1"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 2"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 3"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 4"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 5"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 6"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 7"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 8"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 1"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 2"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 3"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 4"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-    <tds-table-body-row>
-      <tds-body-cell
-        cell-value="Test value 5"
-        cell-key="truck"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 6"
-        cell-key="driver"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 7"
-        cell-key="country"
-        disable-padding="false"
-      ></tds-body-cell>
-      <tds-body-cell
-        cell-value="Test value 8"
-        cell-key="mileage"
-        disable-padding="false"
-      ></tds-body-cell>
-    </tds-table-body-row>
-  </tds-table-body>
-</tds-table>
-    )
-}
+  return (
+    <tds-table
+      no-min-width
+      vertical-dividers="false"
+      compact-design="false"
+      enable-responsive
+    >
+      <tds-table-header>
+        <tds-header-cell
+          column-key="truck"
+          column-title="Truck type"
+        ></tds-header-cell>
+        <tds-header-cell
+          column-key="driver"
+          column-title="Driver name"
+        ></tds-header-cell>
+        <tds-header-cell
+          column-key="country"
+          column-title="Country"
+        ></tds-header-cell>
+        <tds-header-cell
+          column-key="mileage"
+          column-title="Mileage"
+          text-align="right"
+        ></tds-header-cell>
+      </tds-table-header>
+      <tds-table-body>
+        <tds-table-body-row>
+          <tds-body-cell cell-key="truck">
+            <div>This colum uses slots.</div>
+          </tds-body-cell>
+          <tds-body-cell
+            cell-value="This column uses the cell value prop."
+            cell-key="driver"
+          ></tds-body-cell>
+          <tds-body-cell cell-value="Sweden" cell-key="country"></tds-body-cell>
+          <tds-body-cell cell-value="4000" cell-key="mileage"></tds-body-cell>
+        </tds-table-body-row>
+        <tds-table-body-row>
+          <tds-body-cell cell-key="truck">
+            <div>This colum uses slots.</div>
+          </tds-body-cell>
+          <tds-body-cell
+            cell-value="This column uses the cell value prop."
+            cell-key="driver"
+          ></tds-body-cell>
+          <tds-body-cell
+            cell-value="Germany"
+            cell-key="country"
+          ></tds-body-cell>
+          <tds-body-cell cell-value="260" cell-key="mileage"></tds-body-cell>
+        </tds-table-body-row>
+        <tds-table-body-row>
+          <tds-body-cell cell-key="truck">
+            <div>This colum uses slots.</div>
+          </tds-body-cell>
+          <tds-body-cell
+            cell-value="This column uses the cell value prop."
+            cell-key="driver"
+          ></tds-body-cell>
+          <tds-body-cell cell-value="Norway" cell-key="country"></tds-body-cell>
+          <tds-body-cell cell-value="5889" cell-key="mileage"></tds-body-cell>
+        </tds-table-body-row>
+      </tds-table-body>
+    </tds-table>
+  );
+};
 
 export default BasicTable;
