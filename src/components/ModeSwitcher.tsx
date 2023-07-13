1c7b32d682226e0a7a9912ad085f4b149424a012
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2023-07-13 16:46:33 +0200

    docs: document how to do proper event listener removal (#104)

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -1,15 +1,37 @@
-import { Dispatch, SetStateAction } from "react";
+import { TdsToggleCustomEvent } from '@scania/tegel';
+import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
+
+type TdsToggleEvent = TdsToggleCustomEvent<{
+  toggleId: string;
+  checked: boolean;
+}>;
 
 interface ModeSwitcherProps {
-  mode: "Light" | "Dark";
-  setMode: Dispatch<SetStateAction<"Light" | "Dark">>;
+  mode: 'Light' | 'Dark';
+  setMode: Dispatch<SetStateAction<'Light' | 'Dark'>>;
 }
 const ModeSwitcher = ({ mode, setMode }: ModeSwitcherProps) => {
+  const toggleRef = useRef<HTMLTdsToggleElement>(null);
+
+  useEffect(() => {
+    const toggleEl = toggleRef.current as HTMLTdsToggleElement;
+
+    const toggleMode = (event: Event) => {
+      const toggleEvent = event as TdsToggleEvent;
+
+      setMode(toggleEvent.detail.checked ? 'Dark' : 'Light');
+    };
+
+    toggleEl.addEventListener('tdsToggle', toggleMode);
+
+    return () => {
+      toggleEl.removeEventListener('tdsToggle', toggleMode);
+    };
+  }, [setMode]);
+
   return (
     <div className="mode-switcher">
-      <tds-toggle ref={toggle => toggle?.addEventListener('tdsToggle', () => {
-        setMode(mode === "Light" ? "Dark" : "Light")
-      })}>
+      <tds-toggle ref={toggleRef}>
         <div slot="label">{mode} mode</div>
       </tds-toggle>
     </div>
