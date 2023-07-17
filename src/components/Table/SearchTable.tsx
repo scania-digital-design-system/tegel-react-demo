813cafee21eedbaa14c56efa255d8448ce815fab
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2023-07-17 16:06:20 +0200

    fix(table): slot, bodyData, events improvements (#112)

--- src/components/Table/SearchTable.tsx
+++ src/components/Table/SearchTable.tsx
@@ -1,46 +1,60 @@
+import { useEffect, useRef } from 'react';
+import exampleData from './example-data.json';
+
 interface SearchTableProps {
   cancelEvent?: boolean;
 }
 
 const SearchTable = ({ cancelEvent }: SearchTableProps) => {
+  const searchTable = useRef<HTMLTdsTableElement>(null);
+  const searchTableBody = useRef<HTMLTdsTableBodyElement>(null);
+
+  useEffect(() => {
+    const searchTableElement = searchTable?.current;
+    const searchTableBodyElement = searchTableBody?.current;
+    const handleSortEvent = (event: Event) => {
+      if (cancelEvent) {
+        event?.preventDefault();
+      }
+    };
+
+    if (searchTableBodyElement) {
+      searchTableBodyElement.bodyData = exampleData;
+    }
+
+    if (searchTableElement) {
+      searchTableElement.addEventListener('tdsFilterChange', handleSortEvent);
+    }
+    return () => {
+      if (searchTableElement) {
+        searchTableElement.removeEventListener('tdsFilterChange', handleSortEvent);
+      }
+    };
+  });
+
   return (
     <tds-table
       no-min-width
       vertical-dividers="false"
       compact-design="false"
       enable-responsive
-      ref={(element) =>
-        element?.addEventListener("tdsFilterChange", (event) => {
-          if (cancelEvent) {
-            event?.preventDefault();
-          }
-        })
-      }
+      ref={searchTable}
     >
-      <tds-table-toolbar
-        table-title="Filter"
-        enable-filtering
-      ></tds-table-toolbar>
+      <tds-table-toolbar table-title="Filter" enable-filtering></tds-table-toolbar>
       <tds-table-header>
-        <tds-header-cell
-          column-key="truck"
-          column-title="Truck type"
-        ></tds-header-cell>
-        <tds-header-cell
-          column-key="driver"
-          column-title="Driver name"
-        ></tds-header-cell>
-        <tds-header-cell
-          column-key="country"
-          column-title="Country"
-        ></tds-header-cell>
+        <tds-header-cell column-key="truck" column-title="Truck type"></tds-header-cell>
+        <tds-header-cell column-key="driver" column-title="Driver name"></tds-header-cell>
+        <tds-header-cell column-key="country" column-title="Country"></tds-header-cell>
         <tds-header-cell
           column-key="mileage"
           column-title="Mileage"
           text-align="right"
         ></tds-header-cell>
       </tds-table-header>
-      <tds-table-body body-data='[{"truck":"L-series","driver":"Sonya Bruce","country":"Brazil","mileage":123987},{"truck":"P-series","driver":"Guerra Bowman","country":"Sweden","mileage":2000852},{"truck":"G-series","driver":"Ferrell Wallace","country":"Germany","mileage":564},{"truck":"R-series","driver":"Cox Burris","country":"Spain","mileage":1789357},{"truck":"S-series","driver":"Montgomery Cervantes","country":"Croatia","mileage":65},{"truck":"L-series","driver":"Sheryl Nielsen","country":"Greece","mileage":365784},{"truck":"G-series","driver":"Benton Gomez","country":"France","mileage":80957}]'></tds-table-body>
+      <tds-table-body
+        no-result-message="The query did not match any data."
+        ref={searchTableBody}
+      ></tds-table-body>
     </tds-table>
   );
 };
--- src/components/Table/SortableTable.tsx
+++ src/components/Table/SortableTable.tsx
@@ -1,48 +1,62 @@
+import { useEffect, useRef } from 'react';
+import exampleData from './example-data.json';
+
 interface SortableTableProps {
-    cancelEvent?: boolean,
+  cancelEvent?: boolean;
 }
 
+const SortableTable = ({ cancelEvent }: SortableTableProps) => {
+  const sortableTable = useRef<HTMLTdsTableElement>(null);
+  const sortableTableBody = useRef<HTMLTdsTableBodyElement>(null);
 
-const SortableTable = ({cancelEvent}: SortableTableProps) => {
- return(
+  useEffect(() => {
+    const handleSortEvent = (event: Event) => {
+      if (cancelEvent) {
+        event?.preventDefault();
+      }
+    };
+
+    const sortableTableElement = sortableTable?.current;
+    const sortableTableBodyElement = sortableTableBody?.current;
+
+    if (sortableTableBodyElement) {
+      sortableTableBodyElement.bodyData = exampleData;
+    }
+
+    if (sortableTableElement) {
+      sortableTableElement.addEventListener('tdsSortChange', handleSortEvent);
+    }
+
+    return () => {
+      if (sortableTableElement) {
+        sortableTableElement.removeEventListener('tdsSortChange', handleSortEvent);
+      }
+    };
+  });
+
+  return (
     <tds-table
-    ref={(element) => element?.addEventListener('tdsSortChange', (event) => {
-        if(cancelEvent){
-            event?.preventDefault()
-        }
-    })}
-    no-min-width
-    enable-responsive
-    vertical-dividers="false"
-    compact-design="false"
-  >
-    <tds-table-toolbar table-title="Sorting"></tds-table-toolbar>
-    <tds-table-header>
-      <tds-header-cell
-        column-key="truck"
-        column-title="Truck type"
-        sortable
-      ></tds-header-cell>
-      <tds-header-cell
-        column-key="driver"
-        column-title="Driver name"
-        sortable
-      ></tds-header-cell>
-      <tds-header-cell
-        column-key="country"
-        column-title="Country"
-        sortable
-      ></tds-header-cell>
-      <tds-header-cell
-        column-key="mileage"
-        column-title="Mileage"
-        sortable
-        text-align="right"
-      ></tds-header-cell>
-    </tds-table-header>
-    <tds-table-body enable-dummy-data></tds-table-body>
-  </tds-table>
- );
+      ref={sortableTable}
+      no-min-width
+      enable-responsive
+      vertical-dividers="false"
+      compact-design="false"
+    >
+      <tds-table-toolbar table-title="Sorting"></tds-table-toolbar>
+      <tds-table-header>
+        <tds-header-cell column-key="truck" column-title="Truck type" sortable></tds-header-cell>
+        <tds-header-cell column-key="driver" column-title="Driver name" sortable></tds-header-cell>
+        <tds-header-cell column-key="country" column-title="Country" sortable></tds-header-cell>
+        <tds-header-cell
+          column-key="mileage"
+          column-title="Mileage"
+          sortable
+          text-align="right"
+        ></tds-header-cell>
+      </tds-table-header>
+      <tds-table-body ref={sortableTableBody}></tds-table-body>
+    </tds-table>
+  );
 };
 
 export default SortableTable;
