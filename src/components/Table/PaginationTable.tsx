6eefcefc112488b30ecc5c7158bcd5e77962b81d
mJarsater <martin.jarsater@hotmail.com>
2023-08-18 09:36:32 +0200

    added pagination example

--- src/components/Table/PaginationTable.tsx
+++ src/components/Table/PaginationTable.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from 'react';
 import exampleData from './example-data.json';
 
 const PaginationTable = () => {
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const rowsPerPage = 2;
   const [data, setData] = useState(exampleData.slice(page, page + rowsPerPage));
   const paginationTable = useRef<HTMLTdsTableElement>(null);
@@ -11,45 +11,43 @@ const PaginationTable = () => {
     const paginationTableElement = paginationTable?.current;
 
     const handlePaginationEvent = (event: any) => {
-      setPage(event.detail.paginationValue);
-      const startIndex = (event.detail.paginationValue - 1) * rowsPerPage;
-      const endIndex = startIndex + rowsPerPage;
-      const updatedData = exampleData.slice(startIndex, endIndex);
-      setData(updatedData)
+      setPage(event.detail.paginationValue)
     };
 
-    paginationTableElement?.addEventListener('tdsPagination', handlePaginationEvent);
+    paginationTableElement?.addEventListener('tdsPageChange', handlePaginationEvent);
     return () => {
-      paginationTableElement?.removeEventListener('tdsPagination', handlePaginationEvent);
+      paginationTableElement?.removeEventListener('tdsPageChange', handlePaginationEvent);
     };
-  }, []);
+  });
+
+  useEffect(() => {
+    const start = page * rowsPerPage;
+    setData(exampleData.slice(start, start + rowsPerPage));
+  }, [page]);
 
   return (
-    <tds-table ref={paginationTable} responsive no-min-width>
+    <tds-table ref={paginationTable} vertical-dividers="false" compact-design="false" responsive no-min-width>
       <tds-table-header>
-        <tds-header-cell cell-key="truck" cell-value="Truck type"></tds-header-cell>
-        <tds-header-cell cell-key="driver" cell-value="Driver name"></tds-header-cell>
-        <tds-header-cell cell-key="country" cell-value="Country"></tds-header-cell>
+        <tds-header-cell column-key="truck" column-title="Truck type"></tds-header-cell>
+        <tds-header-cell column-key="driver" column-title="Driver name"></tds-header-cell>
+        <tds-header-cell column-key="country" column-title="Country"></tds-header-cell>
         <tds-header-cell
-          cell-key="mileage"
-          cell-value="Mileage"
+          column-key="mileage"
+          column-title="Mileage"
           text-align="right"
         ></tds-header-cell>
       </tds-table-header>
       <tds-table-body>
         {data.map((object, index) => (
           <tds-table-body-row key={index}>
-            <tds-body-cell>{object.truck}</tds-body-cell>
-            <tds-body-cell>{object.driver}</tds-body-cell>
             <tds-body-cell>{object.country}</tds-body-cell>
-            <tds-body-cell style={{ textAlign: 'right' }} >{object.mileage}</tds-body-cell>
+            <tds-body-cell>{object.driver}</tds-body-cell>
+            <tds-body-cell>{object.mileage}</tds-body-cell>
+            <tds-body-cell>{object.truck}</tds-body-cell>
           </tds-table-body-row>
         ))}
       </tds-table-body>
-      <tds-table-footer
-        pagination
-        pages={Math.ceil(exampleData.length / rowsPerPage)}
-      ></tds-table-footer>
+      <tds-table-footer pagination pages={Math.floor((exampleData.length / rowsPerPage))}></tds-table-footer>
     </tds-table>
   );
 };
