678343dda5e710c30009a4905ef480327f8279ca
mJarsater <martin.jarsater@hotmail.com>
2023-08-30 09:47:17 +0200

    propper typing on events and cleanup

--- src/components/TabsButtons.tsx
+++ src/components/TabsButtons.tsx
@@ -1,10 +1,46 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import TabsPanels from "./TabsPanels";
 
 const TabsButtons = () => {
   const [folderTabIndex, setFolderTabIndex] = useState(0);
   const [inlineTabIndex, setInlineTabIndex] = useState(0);
   const [navigationTabIndex, setNavigationTabIndex] = useState(0);
+  const folderTabsRef = useRef<HTMLTdsFolderTabsElement>(null)
+  const inlineTabsRef = useRef<HTMLTdsInlineTabsElement>(null)
+  const navigationTabsRef = useRef<HTMLTdsNavigationTabsElement>(null)
+
+
+  useEffect(() => {
+    const folderTabsElement = folderTabsRef.current
+    const inlineTabsElement = inlineTabsRef.current
+    const navigationTabsElement = navigationTabsRef.current
+
+
+    folderTabsElement?.addEventListener("tdsChange", (event: TdsChangeEvent) => {
+      setFolderTabIndex(event.detail.selectedTabIndex);
+    })
+    inlineTabsElement?.addEventListener("tdsChange", (event: TdsChangeEvent) => {
+      setInlineTabIndex(event.detail.selectedTabIndex);
+    })
+    navigationTabsElement?.addEventListener("tdsChange", (event: TdsChangeEvent) => {
+      setNavigationTabIndex(event.detail.selectedTabIndex);
+    })
+
+
+    return () => {
+      folderTabsElement?.removeEventListener("tdsChange", (event: TdsChangeEvent) => {
+        setFolderTabIndex(event.detail.selectedTabIndex);
+      })
+      inlineTabsElement?.removeEventListener("tdsChange", (event: TdsChangeEvent) => {
+        setFolderTabIndex(event.detail.selectedTabIndex);
+      })
+      navigationTabsElement?.removeEventListener("tdsChange", (event: TdsChangeEvent) => {
+        setFolderTabIndex(event.detail.selectedTabIndex);
+      })
+    }
+
+  })
+
   return (
     <div>
       <h3>Tabs with buttons.</h3>
@@ -14,13 +50,7 @@ const TabsButtons = () => {
       <div className="tds-u-mb3 tabs">
         <tds-folder-tabs
           mode-variant="secondary"
-          ref={(element) =>
-            element?.addEventListener("tdsChange", (event: any) => {
-              setFolderTabIndex(event.detail.selectedTabIndex);
-              console.log(event);
-            })
-          }
-        >
+          ref={folderTabsRef}>
           <tds-folder-tab>
             <button>First tab</button>
           </tds-folder-tab>
@@ -39,12 +69,7 @@ const TabsButtons = () => {
       <div className="tds-u-mb3 tabs">
         <tds-inline-tabs
           mode-variant="secondary"
-          ref={(element) =>
-            element?.addEventListener("tdsChange", (event: any) => {
-              setInlineTabIndex(event.detail.selectedTabIndex);
-              console.log(event);
-            })
-          }
+          ref={inlineTabsRef}
         >
           <tds-inline-tab>
             <button>First tab</button>
@@ -64,12 +89,7 @@ const TabsButtons = () => {
       <div className="tds-u-mb3 tabs">
         <tds-navigation-tabs
           mode-variant="secondary"
-          ref={(element) =>
-            element?.addEventListener("tdsChange", (event: any) => {
-              setNavigationTabIndex(event.detail.selectedTabIndex);
-              console.log(event);
-            })
-          }
+          ref={navigationTabsRef}
         >
           <tds-navigation-tab>
             <button>First tab</button>
