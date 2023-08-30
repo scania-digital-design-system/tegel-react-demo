06ef3a40ca0ebff9a3703fc5c87d4e6069ec9919
mJarsater <martin.jarsater@hotmail.com>
2023-08-30 08:52:03 +0200

    propper typing on events and cleanup

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -1,10 +1,6 @@
 import { TdsToggleCustomEvent } from '@scania/tegel';
 import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
 
-type TdsToggleEvent = TdsToggleCustomEvent<{
-  toggleId: string;
-  checked: boolean;
-}>;
 
 interface ModeSwitcherProps {
   mode: 'Light' | 'Dark';
@@ -16,10 +12,9 @@ const ModeSwitcher = ({ mode, setMode }: ModeSwitcherProps) => {
   useEffect(() => {
     const toggleEl = toggleRef.current as HTMLTdsToggleElement;
 
-    const toggleMode = (event: Event) => {
-      const toggleEvent = event as TdsToggleEvent;
+    const toggleMode = (event: TdsToggleEvent) => {
 
-      setMode(toggleEvent.detail.checked ? 'Dark' : 'Light');
+      setMode(event.detail.checked ? 'Dark' : 'Light');
     };
 
     toggleEl.addEventListener('tdsToggle', toggleMode);
