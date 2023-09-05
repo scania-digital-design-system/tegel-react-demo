233c289754c24de5d990adc351cddbd5f1062b90
Tim <tim.romberg@scania.com>
2023-09-05 16:12:28 +0200

    feat: Notfound page added

---
+++ src/NotFound.tsx
@@ -0,0 +1,19 @@
+import './NotFound.scss';
+
+const NotFound = () => {
+  return (
+    <div className="error-page">
+	    <div className="error-page__wrapper">
+        <div className="error-page__icon-wrapper">
+            <tds-icon name="link_broken" size="24"></tds-icon>
+        </div>
+        <div className="error-page__info">
+          <h2 className="tds-headline-04">The requested page could not be found</h2>
+          <p className='tds-body-01'>The page could not be found. You may have clicked on an incorrect link or mistyped the URL. It is also possible that we have renamed, moved or deleted the content. </p>
+      </div>
+	  </div>
+  </div>
+  );
+};
+
+export default NotFound;
--- src/index.tsx
+++ src/index.tsx
@@ -1,106 +1,126 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import './register-webcomponents';
+  import React from 'react';
+  import ReactDOM from 'react-dom/client';
+  import { createBrowserRouter, Outlet, RouteObject, RouterProvider } from 'react-router-dom';
+  import './index.css';
+  import reportWebVitals from './reportWebVitals';
+  import './register-webcomponents';
 
-import App from './App';
-import WebComponents from './pages/WebComponents/WebComponents';
-import TextPage from './pages/TextPage/TextPage';
-import TablePage from './pages/TablePage';
-import FormPage from './pages/FormPages/FormPage';
-import TabsLinkPage from './pages/TabsLinkPage';
-import TabsButtonPage from './pages/TabsButtonPage';
-import About from './pages/About';
-import SettingsPage from './pages/SettingsPage/SettingsPage';
-import TextSubpage from './pages/TextSubpage';
-import StepperPage from './pages/StepperPage';
-import NotificationPage from './pages/NotificationPage';
+  import App from './App';
+  import WebComponents from './pages/WebComponents/WebComponents';
+  import TextPage from './pages/TextPage/TextPage';
+  import TablePage from './pages/TablePage';
+  import FormPage from './pages/FormPages/FormPage';
+  import TabsLinkPage from './pages/TabsLinkPage';
+  import TabsButtonPage from './pages/TabsButtonPage';
+  import About from './pages/About';
+  import SettingsPage from './pages/SettingsPage/SettingsPage';
+  import TextSubpage from './pages/TextSubpage';
+  import StepperPage from './pages/StepperPage';
+  import NotificationPage from './pages/NotificationPage';
+  import NotFound from './NotFound';
+  import MainLayout from './MainLayout';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '',
-        element: <WebComponents />,
-      },
-      {
-        path: 'web-components',
-        element: <WebComponents />,
-      },
-      {
-        path: 'table',
-        element: <TablePage />,
-      },
-      {
-        path: 'form',
-        element: <FormPage />,
-      },
-      {
-        path: 'about',
-        element: <About />,
-      },
-      {
-        path: 'stepper',
-        element: <StepperPage />,
-      },
-      {
-        path: 'tabs-buttons',
-        element: <TabsButtonPage />,
-      },
-      {
-        path: 'tabs-buttons',
-        element: <TabsButtonPage />,
-      },
-      {
-        path: 'tabs-links',
-        element: <TabsLinkPage />,
-        children: [
-          {
-            path: 'first-tab',
-            element: <TabsLinkPage />,
-          },
-          {
-            path: 'second-tab',
-            element: <TabsLinkPage />,
-          },
-          {
-            path: 'third-tab',
-            element: <TabsLinkPage />,
-          },
-        ],
-      },
-      {
-        path: 'text',
-        element: <TextPage />,
-      },
-      {
-        path: 'text/subpage-with-a-very-long-title',
-        element: <TextSubpage />,
-      },
-      {
-        path: 'settings',
-        element: <SettingsPage />,
-      },
-      {
-        path: 'notifications',
-        element: <NotificationPage />,
-      },
-    ],
-  },
-]);
+  export const mainRoutes: RouteObject[] = [
+    {
+      path: '/',
+      element: <App />,
+      children: [
+        {
+          path: '',
+          element: <WebComponents />,
+        },
+        {
+          path: 'web-components',
+          element: <WebComponents />,
+        },
+        {
+          path: 'table',
+          element: <TablePage />,
+        },
+        {
+          path: 'form',
+          element: <FormPage />,
+        },
+        {
+          path: 'about',
+          element: <About />,
+        },
+        {
+          path: 'stepper',
+          element: <StepperPage />,
+        },
+        {
+          path: 'tabs-buttons',
+          element: <TabsButtonPage />,
+        },
+        {
+          path: 'tabs-buttons',
+          element: <TabsButtonPage />,
+        },
+        {
+          path: 'tabs-links',
+          element: <TabsLinkPage />,
+          children: [
+            {
+              path: 'first-tab',
+              element: <TabsLinkPage />,
+            },
+            {
+              path: 'second-tab',
+              element: <TabsLinkPage />,
+            },
+            {
+              path: 'third-tab',
+              element: <TabsLinkPage />,
+            },
+          ],
+        },
+        {
+          path: 'text',
+          element: <TextPage />,
+        },
+        {
+          path: 'text/subpage-with-a-very-long-title',
+          element: <TextSubpage />,
+        },
+        {
+          path: 'settings',
+          element: <SettingsPage />,
+        },
+        {
+          path: 'notifications',
+          element: <NotificationPage />,
+        },
+      ],
+    },
+  ];
+  
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Outlet />, // Use Outlet here to render nested routes
+      children: [
+        ...mainRoutes,
+        {
+          path: '*',
+          element: <MainLayout shouldRenderBreadcrumbs={false} shouldRenderModeSwitcher={false}>
+          <NotFound />
+        </MainLayout>
+        },
+      ],
+    },
+  ]);
+
+  const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>,
+  );
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();
