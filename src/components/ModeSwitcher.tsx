485f31f794a50128ed0502044bf71df5a040d3cf
mJarsater <martin.jarsater@hotmail.com>
2023-09-07 10:31:10 +0200

    mode/variant switcher updates

--- src/components/ModeSwitcher.tsx
+++ src/components/ModeSwitcher.tsx
@@ -1,8 +1,9 @@
 import { Dispatch, SetStateAction, useEffect, useRef } from 'react';
+import { capitalizeFirst } from '../utility';
 
 interface ModeSwitcherProps {
-  mode: 'Light' | 'Dark';
-  setMode: Dispatch<SetStateAction<'Light' | 'Dark'>>;
+  mode: 'on' | 'off';
+  setMode: Dispatch<SetStateAction<'on' | 'off'>>;
 }
 const ModeSwitcher = ({ mode, setMode }: ModeSwitcherProps) => {
   const toggleRef = useRef<HTMLTdsToggleElement>(null);
@@ -11,7 +12,7 @@ const ModeSwitcher = ({ mode, setMode }: ModeSwitcherProps) => {
     const toggleEl = toggleRef.current as HTMLTdsToggleElement;
 
     const toggleMode = (event: TdsToggleEvent) => {
-      setMode(event.detail.checked ? 'Dark' : 'Light');
+      setMode(event.detail.checked ? 'on' : 'off');
     };
 
     toggleEl.addEventListener('tdsToggle', toggleMode);
@@ -23,8 +24,8 @@ const ModeSwitcher = ({ mode, setMode }: ModeSwitcherProps) => {
 
   return (
     <div className="mode-switcher">
-      <tds-toggle ref={toggleRef}>
-        <div slot="label">{mode} mode</div>
+      <tds-toggle headline='Light mode' ref={toggleRef}>
+        <div slot="label">{capitalizeFirst(mode)}</div>
       </tds-toggle>
     </div>
   );
