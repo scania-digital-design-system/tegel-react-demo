9934ba161fd396bd0217583ab00d711555ef45c4
mJarsater <martin.jarsater@hotmail.com>
2023-09-27 16:00:36 +0200

    badge alignement

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -12,20 +12,9 @@ const Badge = () => {
   return (
     <div>
       <div className="tds-headline-02 tds-u-pb1">Badge</div>
+      <div className="tds-u-flex tds-u-gap2 tds-u-flex-dir-col">
       <tds-badge value="1" size="lg"></tds-badge>
-      <br />
-
-      <tds-badge value="-5" size="lg"></tds-badge>
-      <br />
-
-      <tds-button>
-        <span slot="label"> Inbox </span>
-        <span slot="icon">
-          <tds-badge value="1000" size="lg"></tds-badge>
-        </span>
-      </tds-button>
-      <br />
-      <br />
+      <tds-badge value={emailCount} size="lg"></tds-badge>
       <tds-text-field
         type="text"
         size="sm"
@@ -38,28 +27,22 @@ const Badge = () => {
         value={emailCount}
         onInput={handleInput}
       ></tds-text-field>
-      <br />
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="email" size="24px"></tds-icon>
-          <tds-badge value={emailCount} size="lg"></tds-badge>
-        </span>
-      </tds-button>
+      
+      <div className="tds-u-flex tds-u-gap2">
       <tds-button>
         <span slot="icon" className="button-badge-relative">
           <tds-icon name="heart" size="24px"></tds-icon>
           <tds-badge value="5" size="lg"></tds-badge>
         </span>
       </tds-button>
-      <br />
-      <br />
-
       <tds-button>
         <span slot="icon" className="button-badge-relative">
           <tds-icon name="settings" size="24px"></tds-icon>
           <tds-badge value="5" size="sm"></tds-badge>
         </span>
       </tds-button>
+      </div>
+      </div>
     </div>
   );
 };
