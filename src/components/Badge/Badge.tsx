16c1c663bbc0e8a9dfd15a0bb9743ba7c7bee963
mJarsater <62651103+mJarsater@users.noreply.github.com>
2023-09-28 14:14:16 +0200

    fix: badge alignement (#134)
    * badge alignement
    
    * added email button
    
    * fix: badge

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -2,64 +2,53 @@ import { FormEvent, useState } from 'react';
 import './Badge.scss';
 
 const Badge = () => {
-  const [emailCount, setEmailCount] = useState('100');
+  const [emailCount, setEmailCount] = useState(100);
 
   const handleInput = (event: FormEvent) => {
     const target = event.target as HTMLTdsTextFieldElement;
-    setEmailCount(target.value);
+    setEmailCount(target.value ? Number(target.value) : 0);
   };
 
   return (
     <div>
       <div className="tds-headline-02 tds-u-pb1">Badge</div>
-      <tds-badge value="1" size="lg"></tds-badge>
-      <br />
+      <div className="tds-u-flex tds-u-gap2 tds-u-flex-dir-col">
+        <tds-badge value="1" size="lg"></tds-badge>
+        <tds-badge value="-5" size="lg"></tds-badge>
+        <tds-text-field
+          type="text"
+          size="sm"
+          state="success"
+          label="Number of emails"
+          name="emailCount"
+          label-position="outside"
+          no-min-width
+          placeholder="0"
+          value={emailCount.toString()}
+          onInput={handleInput}
+        ></tds-text-field>
 
-      <tds-badge value="-5" size="lg"></tds-badge>
-      <br />
-
-      <tds-button>
-        <span slot="label"> Inbox </span>
-        <span slot="icon">
-          <tds-badge value="1000" size="lg"></tds-badge>
-        </span>
-      </tds-button>
-      <br />
-      <br />
-      <tds-text-field
-        type="text"
-        size="sm"
-        state="success"
-        label="Number of emails"
-        name="emailCount"
-        label-position="outside"
-        no-min-width
-        placeholder="0"
-        value={emailCount}
-        onInput={handleInput}
-      ></tds-text-field>
-      <br />
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="email" size="24px"></tds-icon>
-          <tds-badge value={emailCount} size="lg"></tds-badge>
-        </span>
-      </tds-button>
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="heart" size="24px"></tds-icon>
-          <tds-badge value="5" size="lg"></tds-badge>
-        </span>
-      </tds-button>
-      <br />
-      <br />
-
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="settings" size="24px"></tds-icon>
-          <tds-badge value="5" size="sm"></tds-badge>
-        </span>
-      </tds-button>
+        <div className="tds-u-flex tds-u-gap2">
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="email" size="24px"></tds-icon>
+              <tds-badge value={emailCount.toString()} size="lg"></tds-badge>
+            </span>
+          </tds-button>
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="heart" size="24px"></tds-icon>
+              <tds-badge value="5" size="lg"></tds-badge>
+            </span>
+          </tds-button>
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="settings" size="24px"></tds-icon>
+              <tds-badge value="5" size="sm"></tds-badge>
+            </span>
+          </tds-button>
+        </div>
+      </div>
     </div>
   );
 };
