0b0cd2e65d2c1a2cf490b952bf160f012fbb66d6
mJarsater <martin.jarsater@hotmail.com>
2023-09-28 09:35:04 +0200

    added email button

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -13,35 +13,41 @@ const Badge = () => {
     <div>
       <div className="tds-headline-02 tds-u-pb1">Badge</div>
       <div className="tds-u-flex tds-u-gap2 tds-u-flex-dir-col">
-      <tds-badge value="1" size="lg"></tds-badge>
-      <tds-badge value={emailCount} size="lg"></tds-badge>
-      <tds-text-field
-        type="text"
-        size="sm"
-        state="success"
-        label="Number of emails"
-        name="emailCount"
-        label-position="outside"
-        no-min-width
-        placeholder="0"
-        value={emailCount}
-        onInput={handleInput}
-      ></tds-text-field>
-      
-      <div className="tds-u-flex tds-u-gap2">
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="heart" size="24px"></tds-icon>
-          <tds-badge value="5" size="lg"></tds-badge>
-        </span>
-      </tds-button>
-      <tds-button>
-        <span slot="icon" className="button-badge-relative">
-          <tds-icon name="settings" size="24px"></tds-icon>
-          <tds-badge value="5" size="sm"></tds-badge>
-        </span>
-      </tds-button>
-      </div>
+        <tds-badge value="1" size="lg"></tds-badge>
+        <tds-badge value="-5" size="lg"></tds-badge>
+        <tds-text-field
+          type="text"
+          size="sm"
+          state="success"
+          label="Number of emails"
+          name="emailCount"
+          label-position="outside"
+          no-min-width
+          placeholder="0"
+          value={emailCount}
+          onInput={handleInput}
+        ></tds-text-field>
+
+        <div className="tds-u-flex tds-u-gap2">
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="email" size="24px"></tds-icon>
+              <tds-badge value={emailCount} size="lg"></tds-badge>
+            </span>
+          </tds-button>
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="heart" size="24px"></tds-icon>
+              <tds-badge value="5" size="lg"></tds-badge>
+            </span>
+          </tds-button>
+          <tds-button>
+            <span slot="icon" className="button-badge-relative">
+              <tds-icon name="settings" size="24px"></tds-icon>
+              <tds-badge value="5" size="sm"></tds-badge>
+            </span>
+          </tds-button>
+        </div>
       </div>
     </div>
   );
