ed16573bffd595e92ea860162fc194eda5d55a16
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-10-11 15:28:46 +0200

    feat(datetime): implement React wrappers

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -1,11 +1,15 @@
+import {
+  TdsDatetime
+} from "@scania/tegel-react";
+
 const Datetime = () => {
   return (
     <>
       <div className="tds-headline-02 tds-u-pb1">Datetime</div>
       <div className="tds-u-flex tds-u-flex-dir-col tds-u-gap2">
-        <tds-datetime type="datetime-local"></tds-datetime>
-        <tds-datetime type="date"></tds-datetime>
-        <tds-datetime type="time"></tds-datetime>
+        <TdsDatetime type="datetime-local"></TdsDatetime>
+        <TdsDatetime type="date"></TdsDatetime>
+        <TdsDatetime type="time"></TdsDatetime>
       </div>
     </>
   );
