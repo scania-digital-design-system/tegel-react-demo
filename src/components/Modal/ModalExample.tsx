4cd7bc89ca0fb8b2e112fd826d0b1c9974e8389e
Lindqvist Nathalie <nathalie.lindqvist@scania.com>
2023-10-12 08:30:39 +0200

    feat(modal): implement React wrappers

--- src/components/Modal/ModalExample.tsx
+++ src/components/Modal/ModalExample.tsx
@@ -1,3 +1,7 @@
+import {
+  TdsButton,
+  TdsIcon
+} from "@scania/tegel-react";
 import Modal from './Modal';
 
 const ModalExample = () => {
@@ -5,16 +9,16 @@ const ModalExample = () => {
     <>
       <div className="tds-headline-02 tds-u-pb1">Modal</div>
       <div className="tds-u-flex tds-u-gap1">
-        <tds-button id="sm-modal-button" size='sm' text="Small Modal"></tds-button>
+        <TdsButton id="sm-modal-button" size='sm' text="Small Modal"></TdsButton>
         <Modal prevent size="sm" selector="#sm-modal-button" />
-        <tds-button id="md-modal-button" size='md' text="Medium Modal"></tds-button>
+        <TdsButton id="md-modal-button" size='md' text="Medium Modal"></TdsButton>
         <Modal size="md" selector="#md-modal-button" />
-        <tds-button id="lg-modal-button" size='lg' text="Large Modal"></tds-button>
+        <TdsButton id="lg-modal-button" size='lg' text="Large Modal"></TdsButton>
         <Modal size="lg" selector="#lg-modal-button" />
       </div>
 
       <p>Modals can also be opened programatically, see it in action by hovering the Icon below.</p>
-      <tds-icon
+      <TdsIcon
         onMouseEnter={() => {
           (
             document.querySelector(`[selector="hover-modal-button"]`) as HTMLTdsModalElement
@@ -22,7 +26,7 @@ const ModalExample = () => {
         }}
         name="truck"
         size="20px"
-      ></tds-icon>
+      ></TdsIcon>
       <Modal size="sm" selector="hover-modal-button" />
     </>
   );
