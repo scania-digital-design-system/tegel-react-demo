fe3a27dc5cf7bb31095042d61dfb16fee9650b64
mJarsater <martin.jarsater@hotmail.com>
2023-10-13 14:53:55 +0200

    fix: updated Spinner component to use React wrappers

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,11 +1,13 @@
+import { TdsSpinner } from "@scania/tegel-react";
+
 const Spinner = () => {
   return (
     <>
       <div className="tds-headline-02 tds-u-pb1">Spinner</div>
       <div className="tds-headline-03 tds-u-pb1">Standard</div>
-      <tds-spinner variant="standard"></tds-spinner>
+      <TdsSpinner variant="standard"></TdsSpinner>
       <div className="tds-headline-03 tds-u-pb1">Inverted</div>
-      <tds-spinner variant="inverted"></tds-spinner>
+      <TdsSpinner variant="inverted"></TdsSpinner>
     </>
   );
 };
