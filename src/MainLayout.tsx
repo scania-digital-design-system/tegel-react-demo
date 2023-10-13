bab89fded616a7bcfdd28838ba71e3d72eacbba0
mJarsater <martin.jarsater@hotmail.com>
2023-10-13 15:13:51 +0200

    fix: updated MainLayout Component to use React wrappers

--- src/MainLayout.tsx
+++ src/MainLayout.tsx
@@ -6,6 +6,7 @@ import Header from './components/Navigation/Header';
 import SideMenu from './components/Navigation/SideMenu';
 import { createContext } from 'react';
 import AppBreadcrumbs from './components/Navigation/AppBreadcrumbs/AppBreadcrumbs';
+import { TdsBanner, TdsLink } from '@scania/tegel-react';
 
 interface MainLayoutProps {
     children?: React.ReactNode;
@@ -44,16 +45,16 @@ const MainLayout = ({
         <div className={`App mode-wrapper tds-mode-${lightMode === 'on' ? 'light' : 'dark'}`}>
             <div className={`mode-variant-wrapper tds-mode-variant-${primaryVariant === 'on' ? 'primary' : 'secondary'}`}>
                 <UserContext.Provider value={userContextValue}>
-                    <tds-banner variant="information" icon="info" header="React demo">
+                    <TdsBanner variant="information" icon="info" header="React demo">
                         <div slot="subheader">
                             This is a demo page in React using{' '}
-                            <tds-link style={{ display: 'inline-block' }}>
+                            <TdsLink style={{ display: 'inline-block' }}>
                                 <a href="https://tegel-storybook.netlify.app/?path=/docs/components--banner">
                                     @scania/tegel
                                 </a>
-                            </tds-link>
+                            </TdsLink>
                         </div>
-                    </tds-banner>
+                    </TdsBanner>
                     <Header pathname={pathname} toggleMobileNav={toggleMobileNav} />
                     <div className="side-menu-and-main">
                         <SideMenu
