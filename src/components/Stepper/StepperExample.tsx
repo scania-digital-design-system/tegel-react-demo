b5b1f765181de86677a09e68539e675cbc0a5b47
mJarsater <martin.jarsater@hotmail.com>
2023-10-13 14:53:48 +0200

    fix: updated Stepper component to use React wrappers

--- src/components/Stepper/StepperExample.tsx
+++ src/components/Stepper/StepperExample.tsx
@@ -1,21 +1,23 @@
+import { TdsStepper, TdsStep } from "@scania/tegel-react";
+
 const StepperExample = () => {
   return (
     <>
       <div className="tds-headline-02 tds-u-pb1">Stepper</div>
-      <tds-stepper size="lg" label-position="below" orientation="horizontal">
-        <tds-step state="success" index="1">
+      <TdsStepper size="lg" labelPosition="below" orientation="horizontal">
+        <TdsStep state="success" index="1">
           <div slot="label">Success step</div>
-        </tds-step>
-        <tds-step state="error" index="2">
+        </TdsStep>
+        <TdsStep state="error" index="2">
           <div slot="label">Error step</div>
-        </tds-step>
-        <tds-step state="current" index="3">
+        </TdsStep>
+        <TdsStep state="current" index="3">
           <div slot="label">Current step</div>
-        </tds-step>
-        <tds-step index="4">
+        </TdsStep>
+        <TdsStep index="4">
           <div slot="label">Upcoming step</div>
-        </tds-step>
-      </tds-stepper>
+        </TdsStep>
+      </TdsStepper>
     </>
   );
 };
