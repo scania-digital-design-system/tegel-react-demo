090c40f81b5548716818d4b2c09f70d284634d5e
Nathalie Lindqvist <nathalie.lindqvist@scania.com>
2024-07-10 11:38:20 +0200

    feat(ag-grid): add editable cells example

--- src/pages/AgGridPage.tsx
+++ src/pages/AgGridPage.tsx
@@ -1,11 +1,13 @@
 import AgGridColumnSearch from '../components/AgGrid/AgGridColumnSearch';
 import AgGridColumnSearchFloating from '../components/AgGrid/AgGridColumnSearchFloating';
+import AgGridEditableCells from '../components/AgGrid/AgGridEditableCells';
 
 const AgGridPage = () => {
   return (
     <div>
       <AgGridColumnSearch />
       <AgGridColumnSearchFloating />
+      <AgGridEditableCells />
     </div>
   );
 };
