be68e5efe0d80295ffd3dd1a8ba5d889c6548602
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-02-08 20:38:54 +0100

    demo of using dropdown with number values

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,5 @@
+import { TdsDivider, TdsDropdown, TdsDropdownOption } from '@scania/tegel-react';
+
 const About = () => {
   return (
     <article>
@@ -5,6 +7,29 @@ const About = () => {
       <p>
         This page is a testing ground and demo for using @scania/tegel-react in a React application.
       </p>
+
+      <TdsDivider />
+
+      <h1>Test dropdown here:</h1>
+      <h2>Open browser console and see logs when selecting dropdown options</h2>
+
+      <TdsDropdown
+        name="dropdown"
+        label="Label text"
+        label-position="outside"
+        placeholder="Placeholder"
+        helper="Helper text"
+        size="lg"
+        multiselect
+        open-direction="auto"
+        normalizeText={true}
+        onTdsChange={(event) => console.log('Selected values:', event.detail.value)}
+      >
+        <TdsDropdownOption value="1">Option 1 (string)</TdsDropdownOption>
+        <TdsDropdownOption value={2}>Option 2 (number)</TdsDropdownOption>
+        <TdsDropdownOption value="3">Option 3 (string)</TdsDropdownOption>
+        <TdsDropdownOption value={4}>Option 4 (number)</TdsDropdownOption>
+      </TdsDropdown>
     </article>
   );
 };
