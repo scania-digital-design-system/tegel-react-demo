e02c95764f21c0e6967059260fdea236f86c92ea
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-02-17 10:29:45 +0100

    (ONLY FOR DEBUGGING, WILL NOT BE MERGED TO MAIN BRANCH) add code for debugging to About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,12 +1,36 @@
-const About = () => {
+import { TdsButton, TdsDropdown, TdsDropdownOption } from '@scania/tegel-react';
+import { useForm } from 'react-hook-form';
+
+interface Request {
+  identifier: number;
+}
+
+const FormComponent = () => {
+  const { register, handleSubmit } = useForm<Request>();
+
   return (
-    <article>
-      <h3>About this page</h3>
-      <p>
-        This page is a testing ground and demo for using @scania/tegel-react in a React application.
-      </p>
-    </article>
+    <form
+      onSubmit={handleSubmit((data: Request) => {
+        console.log(`value: ${data.identifier}, type: ${typeof data.identifier}`);
+      })}
+    >
+      <TdsDropdown label="Identifier" labelPosition="outside" initialValue={1}>
+        <TdsDropdownOption value={1} {...register('identifier')}>
+          Option 1
+        </TdsDropdownOption>
+        <TdsDropdownOption value={2} {...register('identifier')}>
+          Option 2
+        </TdsDropdownOption>
+        <TdsDropdownOption value={3} {...register('identifier')}>
+          Option 3
+        </TdsDropdownOption>
+      </TdsDropdown>
+
+      <div>
+        <TdsButton type="submit" text="Submit" />
+      </div>
+    </form>
   );
 };
 
-export default About;
+export default FormComponent;
