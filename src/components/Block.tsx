5970f148273c9ab7e985fa5ecb672c2f202300ca
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2025-02-17 13:27:00 +0100

    chore: testing block fixes

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -16,7 +16,7 @@ const Block = () => {
           Nunc rutrum nibh ut arcu egestas egestas.
         </p>
         <TdsBlock>
-          <h3 className="tds-headline-04">Nested block</h3>
+          <h3 className="tds-headline-04">Nested block 1</h3>
           <p className="tds-detail-03">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nisi ut eleifend
             ultrices. Nunc venenatis maximus sapien, ac bibendum nisl aliquam in. Morbi ac velit et
@@ -26,6 +26,31 @@ const Block = () => {
             Maecenas sit amet rhoncus arcu. Sed sed molestie elit. Nullam in interdum est, vitae
             aliquam ipsum. Nunc rutrum nibh ut arcu egestas egestas.
           </p>
+          <TdsBlock>
+            <h3 className="tds-headline-04">Nested block 2</h3>
+            <p className="tds-detail-03">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nisi ut
+              eleifend ultrices. Nunc venenatis maximus sapien, ac bibendum nisl aliquam in. Morbi
+              ac velit et ligula consectetur interdum. Vestibulum condimentum, augue vitae lobortis
+              rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus. Pellentesque vel
+              vulputate orci, vel lacinia orci. Sed suscipit leo at diam ullamcorper, vitae volutpat
+              neque dapibus. Maecenas sit amet rhoncus arcu. Sed sed molestie elit. Nullam in
+              interdum est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu egestas egestas.
+            </p>
+            <TdsBlock>
+              <h3 className="tds-headline-04">Nested block 3</h3>
+              <p className="tds-detail-03">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum nisi ut
+                eleifend ultrices. Nunc venenatis maximus sapien, ac bibendum nisl aliquam in. Morbi
+                ac velit et ligula consectetur interdum. Vestibulum condimentum, augue vitae
+                lobortis rhoncus, mi est ultricies mi, sed tincidunt magna nibh in lectus.
+                Pellentesque vel vulputate orci, vel lacinia orci. Sed suscipit leo at diam
+                ullamcorper, vitae volutpat neque dapibus. Maecenas sit amet rhoncus arcu. Sed sed
+                molestie elit. Nullam in interdum est, vitae aliquam ipsum. Nunc rutrum nibh ut arcu
+                egestas egestas.
+              </p>
+            </TdsBlock>
+          </TdsBlock>
         </TdsBlock>
       </TdsBlock>
     </div>
