cde36415d60dd402cf7d0ffc1682f04c117aea66
Charles Krook <charles.krook@gmail.com>
2025-03-06 13:58:26 +0100

    feat: update @scania/tegel-react to 1.24.0-a11y-block-beta.1 and enhance About and TablePage components

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,40 @@
+import { TdsBlock, TdsButton } from '@scania/tegel-react';
+
 const About = () => {
   return (
     <article>
       <h3>About this page</h3>
       <p>
-        This page is a testing ground and demo for using @scania/tegel-react in a React application.
+        <TdsBlock>
+          This page is a simple example of a React application created with Create React App. It
+          uses the{' '}
+          <a
+            href="
+          https://www.npmjs.com/package/@scania/tegel-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Tegel React
+          </a>{' '}
+          <TdsBlock>
+            <div>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            </div>
+            <TdsButton
+              type="button"
+              size="lg"
+              animation="fade"
+              variant="primary"
+              text="Button"
+              modeVariant="primary"
+            ></TdsButton>
+            <TdsBlock>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+              <TdsBlock>Test text bla bla bla </TdsBlock>
+            </TdsBlock>
+          </TdsBlock>
+        </TdsBlock>
       </p>
     </article>
   );
--- src/pages/TablePage.tsx
+++ src/pages/TablePage.tsx
@@ -4,10 +4,20 @@ import ExpandTable from '../components/Table/ExpandTable';
 import PaginationTable from '../components/Table/PaginationTable';
 import SortableTable from '../components/Table/SortableTable';
 import FilterTable from '../components/Table/FilterTable';
+import { TdsCard } from '@scania/tegel-react';
 
 const TablePage = () => {
   return (
     <div>
+      <TdsCard
+        header="Card"
+        clickable
+        subheader="Simple Card"
+        cardId="test-card"
+        onTdsClick={(event) => console.log(event.detail.cardId)}
+      >
+        TESTING
+      </TdsCard>
       <div className="tds-headline-02 tds-u-pb1">Basic Table</div>
       <p>This is a basic Table implementation.</p>
       <BasicTable />
@@ -16,6 +26,7 @@ const TablePage = () => {
         This Table uses the batch actions slot and multiselect in order to get data out of the
         table.
       </p>
+
       <BatchActionTable />
       <div className="tds-headline-02 tds-u-pb1 tds-u-pt3">Sortable Table</div>
       <p>This Table is sortable.</p>
