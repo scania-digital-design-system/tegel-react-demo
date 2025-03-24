6336b47ab9daae211f6fa12f45d43ca41e033bb4
Tim Romberg <tim.romberg@scania.com>
2025-03-24 09:30:21 +0100

    fix: testing dropdown bug

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -7,24 +7,17 @@ const Dropdown = () => {
       <TdsDropdown
         name="dropdown"
         label="Label text"
-        label-position="outside"
+        labelPosition="outside"
         placeholder="Placeholder"
         helper="Helper text"
         size="lg"
-        multiselect
-        filter
-        open-direction="auto"
-        normalizeText={true}
+        value="1"
+        openDirection="auto"
       >
-        <TdsDropdownOption value="optión-1">Äptión 1</TdsDropdownOption>
-        <TdsDropdownOption disabled value="optîón-2">
-          Optîon 2
-        </TdsDropdownOption>
-        <TdsDropdownOption value="optiôn-3">Öptiôn 3</TdsDropdownOption>
-        <TdsDropdownOption value="åptiõn-4">Åptiõn 4</TdsDropdownOption>
-        <TdsDropdownOption value="optiöñ-5">Optiöñ 5</TdsDropdownOption>
-        <TdsDropdownOption value="optiôñ-6">Optiôñ 6</TdsDropdownOption>
-        <TdsDropdownOption value="optiõñ-7">Optiõñ 7</TdsDropdownOption>
+        <TdsDropdownOption value="1">Äptión 1</TdsDropdownOption>
+        <TdsDropdownOption value="2">Option 2</TdsDropdownOption>
+        <TdsDropdownOption value="3">Option 3</TdsDropdownOption>
+        <TdsDropdownOption value="4">Option 4</TdsDropdownOption>
       </TdsDropdown>
     </>
   );
