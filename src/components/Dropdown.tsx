3ae10dc3a5b7f967894d280ccce3cc39f51ec164
patrik-nyfeldt <patrik.nyfeldt@umain.com>
2025-04-09 14:15:40 +0200

    feat(dropdown): showcase fix for allowing numbers & zero values

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -11,20 +11,20 @@ const Dropdown = () => {
         placeholder="Placeholder"
         helper="Helper text"
         size="lg"
-        multiselect
         filter
+        value={0}
         open-direction="auto"
-        normalizeText={true}
       >
-        <TdsDropdownOption value="optión-1">Äptión 1</TdsDropdownOption>
-        <TdsDropdownOption disabled value="optîón-2">
-          Optîon 2
+        <TdsDropdownOption value={0}>Option 0</TdsDropdownOption>
+        <TdsDropdownOption value={1}>Option 1</TdsDropdownOption>
+        <TdsDropdownOption disabled value={2}>
+          Option 2
         </TdsDropdownOption>
-        <TdsDropdownOption value="optiôn-3">Öptiôn 3</TdsDropdownOption>
-        <TdsDropdownOption value="åptiõn-4">Åptiõn 4</TdsDropdownOption>
-        <TdsDropdownOption value="optiöñ-5">Optiöñ 5</TdsDropdownOption>
-        <TdsDropdownOption value="optiôñ-6">Optiôñ 6</TdsDropdownOption>
-        <TdsDropdownOption value="optiõñ-7">Optiõñ 7</TdsDropdownOption>
+        <TdsDropdownOption value={3}>Option 3</TdsDropdownOption>
+        <TdsDropdownOption value={4}>Option 4</TdsDropdownOption>
+        <TdsDropdownOption value={5}>Option 5</TdsDropdownOption>
+        <TdsDropdownOption value={6}>Option 6</TdsDropdownOption>
+        <TdsDropdownOption value={7}>Option 7</TdsDropdownOption>
       </TdsDropdown>
     </>
   );
