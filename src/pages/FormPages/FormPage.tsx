5bc8387efc37e3d91346a40ed116f4fc796533bd
Max Skanvik <179694889+max-umain@users.noreply.github.com>
2025-09-26 14:54:58 +0200

    add text fields for testing new attributes in form

--- src/pages/FormPages/FormPage.tsx
+++ src/pages/FormPages/FormPage.tsx
@@ -108,261 +108,61 @@ const FormPage = () => {
               <TdsTextField
                 modeVariant="secondary"
                 name="text-field"
-                label="Full name"
-                labelPosition="outside"
-                placeholder="John Doe"
+                label="required"
+                labelPosition="inside"
+                required
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="type='email'"
+                type="email"
+                labelPosition="inside"
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="type='tel'"
+                type="tel"
+                labelPosition="inside"
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="autocomplete='on'"
+                autocomplete="on"
+                labelPosition="inside"
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="autocomplete='name'"
+                autocomplete="name"
+                labelPosition="inside"
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="autocomplete='tel'"
+                autocomplete="tel"
+                labelPosition="inside"
+              ></TdsTextField>
+
+              <TdsTextField
+                modeVariant="secondary"
+                name="text-field"
+                label="autocomplete='email'"
+                autocomplete="email"
+                labelPosition="inside"
               ></TdsTextField>
             </section>
-
-            <section>
-              <TdsDatetime
-                modeVariant="secondary"
-                label="Date of birth"
-                name="dateOfBirth"
-                type="date"
-                defaultValue="2023-08-01"
-                min="2023-08-01"
-                max="2024-01-31"
-              ></TdsDatetime>
-            </section>
-
-            <section className="side-by-side-input">
-              <TdsTextField
-                modeVariant="secondary"
-                type="text"
-                size="lg"
-                state="success"
-                label="Phone number"
-                name="phoneNumber"
-                labelPosition="outside"
-                noMinWidth
-                placeholder="08 456 32"
-              >
-                <span slot="prefix">
-                  <TdsIcon name="phone" size="16px"></TdsIcon>
-                </span>
-              </TdsTextField>
-
-              <TdsTextField
-                modeVariant="secondary"
-                type="text"
-                size="lg"
-                state={addressValidation ? 'default' : 'error'}
-                label="Address"
-                name="homeAddress"
-                labelPosition="outside"
-                noMinWidth
-                placeholder="Majorvägen 32"
-                helper={addressValidation ? '' : 'Address is mandatory field!'}
-                onTdsInput={(event) => {
-                  const target = event.target as HTMLTdsTextFieldElement;
-                  setAddressValue(target.value);
-                  setTextareaDisabled(!addressValue);
-                }}
-              >
-                <span slot="prefix">
-                  <TdsIcon name="pin" size="16px"></TdsIcon>
-                </span>
-              </TdsTextField>
-            </section>
-
-            <section>
-              <TdsDropdown
-                onTdsChange={handleTdsChange}
-                modeVariant="secondary"
-                name="country"
-                label="Which country you want to select?"
-                labelPosition="outside"
-                placeholder="Country select"
-                size="lg"
-                openDirection="up"
-                defaultValue={'sweden'}
-              >
-                <TdsDropdownOption value="sweden">Sweden</TdsDropdownOption>
-                <TdsDropdownOption disabled value="Finland">
-                  Finland
-                </TdsDropdownOption>
-                <TdsDropdownOption value="norway">Norway</TdsDropdownOption>
-              </TdsDropdown>
-            </section>
-
-            <section>
-              <TdsDropdown
-                ref={norwayDropdownTown}
-                modeVariant="secondary"
-                name="norweiganTown"
-                label="Which towns have you visited in Norway?"
-                labelPosition="outside"
-                placeholder="Norweigan Town"
-                helper=""
-                size="lg"
-                openDirection="auto"
-                multiselect
-                disabled={countrySelected !== 'norway'}
-              >
-                {townDataNorway.map((town) => (
-                  <TdsDropdownOption disabled={town.disabled} value={town.value}>
-                    {town.label}
-                  </TdsDropdownOption>
-                ))}
-              </TdsDropdown>
-            </section>
-
-            <section>
-              <TdsDropdown
-                ref={swedenDropdownTown}
-                modeVariant="secondary"
-                name="swedishTown"
-                label="Select your favourite Swedish town"
-                labelPosition="outside"
-                placeholder="Swedish Town"
-                helper=""
-                size="lg"
-                openDirection="auto"
-                filter
-                disabled={countrySelected !== 'sweden'}
-              >
-                {townDataSweden.map((town) => (
-                  <TdsDropdownOption disabled={town.disabled} value={town.value}>
-                    {town.label}
-                  </TdsDropdownOption>
-                ))}
-              </TdsDropdown>
-            </section>
-
-            <section>
-              <h5>Which offices do you work from?</h5>
-              <TdsCheckbox value="sodertalje321" name="office" checked>
-                <div slot="label">Södertälje 321</div>
-              </TdsCheckbox>
-              <TdsCheckbox value="sergel" name="office">
-                <div slot="label">Sergel</div>
-              </TdsCheckbox>
-              <TdsCheckbox value="homeoffice" name="office">
-                <div slot="label">Home office</div>
-              </TdsCheckbox>
-              <TdsCheckbox value="b260" name="office" disabled>
-                <div slot="label">Building 260</div>
-              </TdsCheckbox>
-              <h5>How long have you been at Scania?</h5>
-              <TdsRadioButton checked value="radio-1" name="yearsAtScania">
-                <div slot="label">0 - 6 months</div>
-              </TdsRadioButton>
-              <TdsRadioButton value="radio-2" name="yearsAtScania">
-                <div slot="label">6 months - 1 year</div>
-              </TdsRadioButton>
-              <TdsRadioButton value="radio-2" name="yearsAtScania">
-                <div slot="label">1 - 2 years</div>
-              </TdsRadioButton>
-              <TdsRadioButton value="radio-2" name="yearsAtScania">
-                <div slot="label">2 - 5 years</div>
-              </TdsRadioButton>
-              <TdsRadioButton value="radio-2" name="yearsAtScania">
-                <div slot="label">+ 5 years</div>
-              </TdsRadioButton>
-            </section>
-
-            <section className="tds-u-mt3">
-              <h5>Textarea</h5>
-              <TdsTextarea
-                modeVariant="secondary"
-                name="occupationalEssay"
-                label="What do you do at Scania? (Minimum 50 chars)"
-                labelPosition="outside"
-                state={occupationEssayState}
-                helper={helperTextOccupationEssayState}
-                disabled={textareaDisabled}
-              ></TdsTextarea>
-            </section>
-
-            <section>
-              <h5>Tell us how you feel about your..</h5>
-              <TdsSlider
-                onTdsChange={() => {
-                  setStressSliderDisabled(false);
-                }}
-                label="..happiness at work"
-                name="happinessAtWork"
-                min="0"
-                value="5"
-                showTickNumbers
-                max="10"
-                ticks="9"
-                snap
-                thumbSize="sm"
-              ></TdsSlider>
-              <TdsSlider
-                onTdsChange={() => {
-                  setWorkLifeSliderDisabled(false);
-                }}
-                label="..stress level"
-                name="stressLevel"
-                min="0"
-                value="5"
-                showTickNumbers
-                max="10"
-                ticks="9"
-                snap
-                disabled={stressSliderDisabled}
-              ></TdsSlider>
-              <TdsSlider
-                label="..work/life balance."
-                name="workLifeBalance"
-                min="0"
-                value="5"
-                showTickNumbers
-                max="10"
-                ticks="9"
-                snap
-                readOnly={workLifeSliderDisabled}
-              ></TdsSlider>
-            </section>
-            <section>
-              <h5>What topics would you like to learn more about?</h5>
-              <div className="chips">
-                <TdsChip value="webDevelopment" type="checkbox" name="insterest">
-                  <div slot="label">Web developement</div>
-                </TdsChip>
-                <TdsChip value="ciCd" type="checkbox" name="insterest">
-                  <div slot="label">CI/CD</div>
-                </TdsChip>
-                <TdsChip value="docker" type="checkbox" name="insterest">
-                  <div slot="label">Docker</div>
-                </TdsChip>
-                <TdsChip value="Kafka" type="checkbox" name="insterest">
-                  <div slot="label">Kafka</div>
-                </TdsChip>
-                <TdsChip value="beDevelopment" type="checkbox" name="insterest">
-                  <div slot="label">Backend development</div>
-                </TdsChip>
-                <TdsChip value="python" type="checkbox" name="insterest">
-                  <div slot="label">Python</div>
-                </TdsChip>
-                <TdsChip value="js" type="checkbox" name="insterest">
-                  <div slot="label">Javascript</div>
-                </TdsChip>
-                <TdsChip value="angular" type="checkbox" name="insterest">
-                  <div slot="label">Angular</div>
-                </TdsChip>
-                <TdsChip value="react" type="checkbox" name="insterest">
-                  <div slot="label">React</div>
-                </TdsChip>
-              </div>
-            </section>
           </TdsBlock>
-          <section>
-            <TdsTooltip placement="top" selector="#anonymously" mouse-over-tooltip="true" show>
-              <p className="tds-detail-05 tds-u-m0 tooltip-paragraph">
-                This option is required in order to submit the form.
-              </p>
-            </TdsTooltip>
-            <TdsToggle id="privacy-toggle" required name="toggle" size="sm">
-              <span slot="label" id="anonymously">
-                Answer anonymously
-                <TdsIcon name="info" size="16px"></TdsIcon>
-              </span>
-            </TdsToggle>
-          </section>
+
           <div className="spinner-container">
             <TdsSpinner size="md" variant="standard"></TdsSpinner>
           </div>
