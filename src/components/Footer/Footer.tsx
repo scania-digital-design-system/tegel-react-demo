9cc427898b00c587927df038c5695deffa47d5dc
Filipa Neto <filipa.neto@scania.com>
2026-08-06 16:39:52 +0100

    fix(footer): add tdsListAriaLabel to footer groups and replace titleText with slot="title"
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,83 +1,87 @@
-import { TdsFooter, TdsFooterGroup, TdsFooterItem, TdsIcon } from '@scania/tegel-react';
+import { TdsFooter, TdsFooterGroup, TdsFooterItem, TdsIcon } from "@scania/tegel-react";
 
 export default function Footer() {
-  return (
-    <TdsFooter>
-      <div slot="top">
-        <TdsFooterGroup title-text="Pages">
-          <TdsFooterItem>
-            <a href="/">Home</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/form">Form</a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-        <TdsFooterGroup title-text="Legals">
-          <TdsFooterItem>
-            <a href="/">Terms & Conditions</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Privacy policy</a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-        <TdsFooterGroup title-text="Design">
-          <TdsFooterItem>
-            <a href="/">Grid system</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Icons</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Usability testing</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">User personas</a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-        <TdsFooterGroup title-text="Support">
-          <TdsFooterItem>
-            <a href="/">Contact</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">FAQ</a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-      </div>
-      <div slot="start">
-        <TdsFooterGroup>
-          <TdsFooterItem>
-            <a href="/">Link text</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Link text</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Link text</a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">Link text</a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-      </div>
-      <div slot="end">
-        <TdsFooterGroup>
-          <TdsFooterItem>
-            <a href="/">
-              <TdsIcon name="truck"></TdsIcon>
-            </a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">
-              <TdsIcon name="truck"></TdsIcon>
-            </a>
-          </TdsFooterItem>
-          <TdsFooterItem>
-            <a href="/">
-              <TdsIcon name="truck"></TdsIcon>
-            </a>
-          </TdsFooterItem>
-        </TdsFooterGroup>
-      </div>
-    </TdsFooter>
-  );
+	return (
+		<TdsFooter>
+			<div slot="top">
+				<TdsFooterGroup tdsListAriaLabel="pages">
+					<span slot="title">Pages</span>
+					<TdsFooterItem>
+						<a href="/">Home</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/form">Form</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+				<TdsFooterGroup tdsListAriaLabel="legals">
+					<span slot="title">Legals</span>
+					<TdsFooterItem>
+						<a href="/">Terms & Conditions</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Privacy policy</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+				<TdsFooterGroup tdsListAriaLabel="design">
+					<span slot="title">Design</span>
+					<TdsFooterItem>
+						<a href="/">Grid system</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Icons</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Usability testing</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">User personas</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+				<TdsFooterGroup tdsListAriaLabel="support">
+					<span slot="title">Support</span>
+					<TdsFooterItem>
+						<a href="/">Contact</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">FAQ</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+			</div>
+			<div slot="start">
+				<TdsFooterGroup tdsListAriaLabel="example">
+					<TdsFooterItem>
+						<a href="/">Link text</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Link text</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Link text</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">Link text</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+			</div>
+			<div slot="end">
+				<TdsFooterGroup tdsListAriaLabel="another-example">
+					<TdsFooterItem>
+						<a href="/">
+							<TdsIcon name="truck"></TdsIcon>
+						</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">
+							<TdsIcon name="truck"></TdsIcon>
+						</a>
+					</TdsFooterItem>
+					<TdsFooterItem>
+						<a href="/">
+							<TdsIcon name="truck"></TdsIcon>
+						</a>
+					</TdsFooterItem>
+				</TdsFooterGroup>
+			</div>
+		</TdsFooter>
+	);
 }
