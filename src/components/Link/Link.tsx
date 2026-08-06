e65603ade9f87b2031d0d82ae13298a91f6cacb4
Filipa Neto <filipa.neto@scania.com>
2026-08-06 16:40:25 +0100

    fix(link): add example of a disabled link
    Co-authored-by: Clara Severino <clara.severino@scania.com>

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,16 +1,22 @@
-import { TdsLink } from '@scania/tegel-react';
+import { TdsLink } from "@scania/tegel-react";
 
 const Link = () => {
-  return (
-    <>
-      <div className="tds-headline-02 tds-u-pb1">Link</div>
-      <TdsLink>
-        <a target="_blank" rel="noreferrer" href="https://tegel.scania.com/home">
-          Tegel
-        </a>
-      </TdsLink>
-    </>
-  );
+	return (
+		<>
+			<div className="tds-headline-02 tds-u-pb1">Link</div>
+			<TdsLink>
+				<a target="_blank" rel="noreferrer" href="https://tegel.scania.com/home">
+					Tegel
+				</a>
+			</TdsLink>
+			<div className="tds-headline-02 tds-u-pb1">Disabled Link</div>
+			<TdsLink disabled>
+				<a target="_blank" rel="noreferrer" href="https://tegel.scania.com/home">
+					Tegel
+				</a>
+			</TdsLink>
+		</>
+	);
 };
 
 export default Link;
