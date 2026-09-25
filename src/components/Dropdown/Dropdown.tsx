4ae93a4cfa51cb81e15d924f674d93d6a73fd606
Filipa Neto <filipa.neto@scania.com>
2026-09-25 12:37:51 +0100

    build(deps): update tegel versions to dropdown-list-beta.1

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -13,7 +13,7 @@ const Dropdown = () => {
 				placeholder="Placeholder"
 				helper="Helper text"
 				size="lg"
-				open-direction="down"
+				open-direction="up"
 				normalizeText={true}
 			>
 				<TdsDropdownOption value="option-1">Option 1</TdsDropdownOption>
